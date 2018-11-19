0da31b698945790954f846a7c0aaab545922ec72
Mario Zamora <mariozamora@Marios-MacBook-Pro-2.local>
2018-11-19 18:56:18 -0500

    Setup typeDefs and resolvers

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
-console.log('GraphQL API')
+import { GraphQLServer } from 'graphql-yoga';
+
+const typeDefs = `
+    type Query {
+        hello: String!
+        name: String!
+        location: String!
+        bio: String!
+    }
+`;
+
+const resolvers = {
+    Query: {
+        hello() {
+            return 'First Query'
+        },
+        name() {
+            return 'Mario Zamora'
+        },
+        location() {
+            return 'World'
+        },
+        bio() {
+            return '...'
+        }
+    }
+};
+
+const server = new GraphQLServer({ typeDefs, resolvers });
+
+server.start(() => console.log(`SERVER RUNNING ON PORT: 4000`) )
